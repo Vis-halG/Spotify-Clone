9924534b12037fc089a1a128de076d8461232062
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2025-09-02 23:42:42 +0530

    Update Display.jsx

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -5,30 +5,39 @@ import DisplayAlbum from './DisplayAlbum'
 import { albumsData } from '../assets/assets'
 
 const Display = () => {
+  const displayRef = useRef(); // Reference to the main display container
+  const location = useLocation(); // React Router hook for current URL info
 
-  const displayRef = useRef(); 
-  const location = useLocation();
+  // Check if current route is an album page
   const isAlbum = location.pathname.includes("album");
+  
+  // Extract album ID from URL if on album page
   const albumId = isAlbum ? location.pathname.slice(-1) : "";
+  
+  // Get background color for the album from data
   const bgColor = albumsData[Number(albumId)].bgColor;
 
-  useEffect(()=>{
+  useEffect(() => {
+    // Change background gradient if on album page, else set default background
     if (isAlbum) {
-      displayRef.current.style.background = `linear-gradient(${bgColor},#121212)`
+      displayRef.current.style.background = `linear-gradient(${bgColor},#121212)`;
+    } else {
+      displayRef.current.style.background = `#121212`;
     }
-    else{
-      displayRef.current.style.background = `#121212`
-    }
-  })
-  
+  });
+
   return (
-    <div ref={displayRef} className='W-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
-        <Routes>
-            <Route path='/' element={<DisplayHome/>}/>
-            <Route path='/album/:id' element={<DisplayAlbum/>}/>
-        </Routes>
+    <div
+      ref={displayRef}
+      className='W-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'
+    >
+      {/* Routing for home and album views */}
+      <Routes>
+        <Route path='/' element={<DisplayHome />} />
+        <Route path='/album/:id' element={<DisplayAlbum />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default Display
+export default Display;
