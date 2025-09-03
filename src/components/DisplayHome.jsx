68aea8d3f58157c69821d2bf3087f3c64931c18d
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2025-09-03 10:02:58 +0530

    Update DisplayHome.jsx

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -17,7 +17,7 @@ const DisplayHome = () => {
           {albumsData.map((item, index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
         </div>
       </div>
-
+ {/* Section for Featured Charts */}
       <div className='mb-4'>
         <h1 className='my-5 font-bold text-2xl'>Today's biggest hits</h1>
         <div className='flex overflow-auto '>
