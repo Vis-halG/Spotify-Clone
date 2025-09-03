7e63a9b6c1bbb8063b7b6dd032b8f3d6e5fed1dd
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2025-09-03 10:11:20 +0530

    Update Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,8 +3,18 @@ import { assets } from '../assets/assets'
 import { PlayerContext } from '../context/PlayerContext'
 
 const Player = () => {
-
-    const { track, seekBar, seekBg, playStatus, play, pause, time, previous, next, seekSong } = useContext(PlayerContext);
+ // Destructure all required player state and functions from the context
+    const { track,
+           seekBar,
+           seekBg, 
+           playStatus, 
+           play,
+           pause,
+           time,
+           previous,
+           next, 
+           seekSong 
+          } = useContext(PlayerContext);
 
     return (
         <div className='h-[10%] bg-black flex justify-between items-center text-white px-4 fixed bottom-0 w-full '>
