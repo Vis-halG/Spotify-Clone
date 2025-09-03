806e41e8cb41ab496994115eacb0465066dc75d7
Vishal Gupta <157860098+Vis-halG@users.noreply.github.com>
2025-09-03 10:04:12 +0530

    Update DisplayHome.jsx

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -20,8 +20,16 @@ const DisplayHome = () => {
  {/* Section for Featured Charts */}
       <div className='mb-4'>
         <h1 className='my-5 font-bold text-2xl'>Today's biggest hits</h1>
-        <div className='flex overflow-auto '>
-          {songsData.map((item, index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+       <div className='flex overflow-auto '>
+          {songsData.map((item, index) => (
+            // Render SongItem for each song in songsData
+            <SongItem 
+              key={index} // Unique key for efficient re-rendering
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />))}
         </div>
       </div>
     </>
